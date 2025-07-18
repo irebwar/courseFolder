d734589924609450ef50b8ff6af2d6b2998d0f6f
Ibrahim Rebwar <ibrahim.racs2022@uoh.edu.iq>
2025-07-18 10:28:40 +0300

    js

---
+++ BizPro/js/main.js
@@ -0,0 +1,41 @@
+// console.log("Hello, Welcome to Full Stack Web Development Course.");
+
+// let score;
+// score = 10;
+// let score1 = "20";
+
+// console.log("Your initial score is: ", score1);
+
+// score = 22;
+// console.log("Your initial score is: ", score);
+
+// const playerName = "Ibarhim";
+// console.log("Plaer Name is: ", playerName);
+
+// playerName = "Rebwar";
+// console.log(playerName);
+
+const courseName = "Full-Stack Web Develompent";
+// let coursePrice = 70000;
+// const inAvailable = true;
+
+// console.log("Coure Name: ", courseName, "(Type: " + typeof courseName + ")");
+// console.log("Coure Price: ", coursePrice, "(Type: " + typeof coursePrice + ")");
+// console.log("Coure Available: ", inAvailable, "(Type: " + typeof inAvailable + ")");
+
+
+function sayHello() {
+	console.log("----------------------");
+	console.log("Welcome to our course!")
+	console.log("----------------------");
+}
+
+sayHello();
+
+function greetUser(username) {
+	console.log("Welcome to: ", username, "Course");
+
+}
+
+// greetUser("Ibrahim");
+greetUser(courseName);
---
+++ js/lesson1/main.js
@@ -0,0 +1,44 @@
+let currentYear = 2025;
+console.log("ئێمە لە ساڵی ", currentYear, "داین");
+
+currentYear = 2026;
+console.log("ساڵیس داهاتوو :", currentYear);
+
+
+const myName = "Ibrahim Rebwar";
+console.log("ناوی من بریتیە لە: ", myName);
+// myName = "Alan";
+// console.log("ناوی من بریتیە لە: ", myName);
+
+
+const birthYear = 2002;
+let age = 2025 - 2002;
+console.log(age);
+
+const courseTitle = "Full Stack Web Development";
+console.log('Name Course: ', courseTitle);
+
+const welcome = `Welcome to: ${courseTitle}`;
+console.log(welcome);
+
+
+const price = 70;
+const quantity = 2;
+const total = price * quantity;
+console.log("Total: ", total);
+console.log(price * quantity);
+
+let isLoggedIn = true;
+let hasAdminAccess = false;
+console.log("Is Login: ", isLoggedIn);
+console.log("Is Admin Access: ", hasAdminAccess);
+
+let userCity;
+console.log("User City: ", userCity);
+
+let selectCourse = null;
+console.log("Selected Coyrse: ", selectCourse);
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
