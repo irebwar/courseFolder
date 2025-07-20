66febf9492156969d1f7464c197bf3645ab7f7de
Ibrahim Rebwar <ibrahim.racs2022@uoh.edu.iq>
2025-07-20 19:00:14 +0300

    update html

--- js/lesson1/main.js
+++ js/lesson1/main.js
@@ -1,44 +1,106 @@
-let currentYear = 2025;
-console.log("ئێمە لە ساڵی ", currentYear, "داین");
+// let currentYear = 2025;
+// console.log("ئێمە لە ساڵی ", currentYear, "داین");
 
-currentYear = 2026;
-console.log("ساڵیس داهاتوو :", currentYear);
+// currentYear = 2026;
+// console.log("ساڵیس داهاتوو :", currentYear);
 
 
-const myName = "Ibrahim Rebwar";
-console.log("ناوی من بریتیە لە: ", myName);
+// const myName = "Ibrahim Rebwar";
+// console.log("ناوی من بریتیە لە: ", myName);
 // myName = "Alan";
 // console.log("ناوی من بریتیە لە: ", myName);
 
 
-const birthYear = 2002;
-let age = 2025 - 2002;
-console.log(age);
+// const birthYear = 2002;
+// let age = 2025 - 2002;
+// console.log(age);
 
-const courseTitle = "Full Stack Web Development";
-console.log('Name Course: ', courseTitle);
+// const courseTitle = "Full Stack Web Development";
+// console.log('Name Course: ', courseTitle);
 
-const welcome = `Welcome to: ${courseTitle}`;
-console.log(welcome);
+// const welcome = `Welcome to: ${courseTitle}`;
+// console.log(welcome);
 
 
-const price = 70;
-const quantity = 2;
-const total = price * quantity;
-console.log("Total: ", total);
-console.log(price * quantity);
+// const price = 70;
+// const quantity = 2;
+// const total = price * quantity;
+// console.log("Total: ", total);
+// console.log(price * quantity);
 
-let isLoggedIn = true;
-let hasAdminAccess = false;
-console.log("Is Login: ", isLoggedIn);
-console.log("Is Admin Access: ", hasAdminAccess);
+// let isLoggedIn = true;
+// let hasAdminAccess = false;
+// console.log("Is Login: ", isLoggedIn);
+// console.log("Is Admin Access: ", hasAdminAccess);
 
-let userCity;
-console.log("User City: ", userCity);
+// let userCity;
+// console.log("User City: ", userCity);
 
-let selectCourse = null;
-console.log("Selected Coyrse: ", selectCourse);
+// let selectCourse = null;
+// console.log("Selected Coyrse: ", selectCourse);
 
-console.log(typeof myName);
-console.log(typeof age);
-console.log(typeof isLoggedIn);
+// console.log(typeof myName);
+// console.log(typeof age);
+// console.log(typeof isLoggedIn);
+
+// console.log("Welcome");
+
+// const drivinAge = 18;
+// let userAge = 12;
+
+// if (userAge >= drivinAge) {
+// 	// (23 >= 18)
+// 	console.log("پیرۆزە!");
+// }
+
+
+// let studentScore = 50;
+// const passingScore = 50;
+
+// if (studentScore >= passingScore) {
+// 	// (45 >= 50)
+// 	console.log("پیرۆزە");
+// } else {
+// 	console.log("بەداخەوە.");
+// }
+
+// const hour = 24;
+
+// if (hour < 12) {
+// 	console.log("بەیانی");
+// } 
+
+// else if (hour < 18) {
+// 	console.log("نیوەڕۆ");
+// }
+
+// else {
+// 	console.log("ئێوارە.");
+// }
+
+// const isLoggedIn = false;
+// const hasSubsciption = false;
+// const isGameFree = false;
+
+// if (isLoggedIn && (!hasSubsciption || isGameFree)) {
+// 	console.log("Welcome");
+// }else {
+// 	console.log("False");
+// }
+
+// if (!isLoggedIn) {
+// 	console.log("تکایە سەرەتا بچۆ ژوورەوە.")
+// }
+
+// let username = "Ibrahim";
+
+// if (username) {
+// 	console.log("Welcome ", username);
+// }
+
+// let emptyInput = "1";
+// if (emptyInput) {
+// 	console.log("is not empty input");
+// }else {
+// 	console.log("Is input empty");
+// }
